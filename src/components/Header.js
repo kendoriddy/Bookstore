db98f0d20e386ef916cc2d3b1e3fbf75bdbdb96b
Kehinde Onifade <onifkay@gmail.com>
2022-09-15 21:15:41 +0100

    Add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import '../css/header.css';
+import { BsPersonFill } from 'react-icons/bs';
 
 const Header = () => (
-  <div>Header</div>
+  <div className="header">
+    <div className="navs">
+      <h3>Bookstore CMS</h3>
+      <nav>BOOKS</nav>
+      <nav>CATEGORIES</nav>
+    </div>
+    <div className="oval">
+      <div className="icon">
+        <BsPersonFill />
+      </div>
+    </div>
+  </div>
 );
 
 export default Header;
