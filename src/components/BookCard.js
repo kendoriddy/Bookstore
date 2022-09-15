73fdfdd8da7a4f1808762a4284b7803815c25253
Kehinde Onifade <onifkay@gmail.com>
2022-09-15 21:49:08 +0100

    First draft of book card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import '../css/card.css';
 
 const BookCard = () => (
-  <div>BookCard</div>
+  <div className="card-container">
+    <div className="left-side">
+      <span className="class-of">Action</span>
+      <h2 className="title">The Hunger Games</h2>
+      <span className="author">Suzanne Collins</span>
+    </div>
+    <div>
+      <button type="button">Comments</button>
+      {' '}
+      |
+      {' '}
+      <button type="button">Remove</button>
+      {' '}
+      |
+      {' '}
+      <button type="button">Edit</button>
+      {' '}
+    </div>
+  </div>
 );
 
 export default BookCard;
