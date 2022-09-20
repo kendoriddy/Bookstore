a49c7c92f378de302990ec3fad658bf20dcbf8be
Kehinde Onifade <onifkay@gmail.com>
2022-09-20 08:44:04 +0100

    Add reducers for book

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -13,3 +13,13 @@ export default function books(state = initialState, action) {
       return state;
   }
 }
+
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  payload: book,
+});
+
+export const removeBook = (book) => ({
+  type: REMOVE_BOOK,
+  payload: book,
+});
