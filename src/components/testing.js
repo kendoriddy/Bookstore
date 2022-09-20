9b9bedff3c52967ac55c328e71324658a5b77f3c
Kehinde Onifade <onifkay@gmail.com>
2022-09-20 11:35:21 +0100

    Add remove book functionalities

--- src/components/testing.js
+++ src/components/testing.js
@@ -1,60 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/Books';
+import { PropTypes } from 'prop-types';
+import { removeBook } from '../redux/books/Books';
 
-export default function AddBook() {
-  const dispatch = useDispatch();
+const Book = (props) => {
+  const dispath = useDispatch();
+  const { title, author, id } = props;
 
-  const [bookValues, setBookValues] = useState({
-    title: '',
-    author: '',
-  });
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setBookValues({
-      ...bookValues,
-      [e.target.name]: e.target.value,
-    });
+  const removeHandler = () => {
+    dispath(removeBook(id));
   };
 
-  const addBookHandler = (e) => {
-    e.preventDefault();
-    if (!bookValues.title.length > 0 || !bookValues.author.length > 0) return;
-    const book = {
-      title: bookValues.title,
-      author: bookValues.author,
-      id: uuidv4(),
-    };
-    dispatch(addBook(book));
-    setBookValues({
-      title: '',
-      author: '',
-    });
-  };
   return (
-    <div>
-      <h1>ADD NEW BOOK</h1>
-      <form onSubmit={addBookHandler}>
-        <input
-          type="text"
-          required
-          value={bookValues.title}
-          onChange={handleChange}
-          placeholder="Book title"
-          name="title"
-        />
-        <input
-          type="text"
-          required
-          value={bookValues.author}
-          onChange={handleChange}
-          placeholder="Book author"
-          name="author"
-        />
-        <button type="submit">ADD BOOK</button>
-      </form>
-    </div>
+    <>
+      <ul>
+        <li>{title}</li>
+        <li>{author}</li>
+        <button type="button" onClick={removeHandler}>Remove</button>
+      </ul>
+
+    </>
   );
-}
+};
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default Book;
