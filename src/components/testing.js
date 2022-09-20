a7294c4f82191c4a97302ca73481cca7142332e0
Kehinde Onifade <onifkay@gmail.com>
2022-09-20 11:48:10 +0100

    Update categories.js

---
+++ src/components/testing.js
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { checkBookStatus } from '../redux/categories/Categories';
+
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const statusChecker = () => {
+    dispatch(checkBookStatus());
+  };
+  return (
+    <div className="check">
+      <button onClick={statusChecker} type="button">
+        Check status
+      </button>
+      <h4>{status}</h4>
+    </div>
+  );
+};
+export default Categories;
