834a43b923b5481ba313c4fd8bed9f796c6cd4b0
Kehinde Onifade <onifkay@gmail.com>
2022-09-20 08:41:10 +0100

    Add action and action creator for books

---
+++ src/redux/books/books.js
@@ -0,0 +1,15 @@
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
+const initialState = [];
+
+export default function books(state = initialState, action) {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+}
