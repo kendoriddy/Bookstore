38b7b4ae92f68221a6dec78a157e1bceefedf731
Kehinde Onifade <onifkay@gmail.com>
2022-09-20 10:20:48 +0100

    Update linters

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,14 +1,16 @@
 const CHECK = 'bookstore/categories/CHECK';
 
-export default function reducer(state = [], action) {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case CHECK:
       return 'under construction';
     default:
       return state;
   }
-}
+};
 
 export const checkBookStatus = () => ({
   type: CHECK,
 });
+
+export default reducer;
