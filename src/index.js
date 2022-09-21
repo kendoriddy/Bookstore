ee06dbb45dfd7ec0c5a2e1f3737ce19b818670ea
Kehinde Onifade <onifkay@gmail.com>
2022-09-21 21:03:49 +0100

    fetched the book list from Bookstore API on the Books page load.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
+import { fetchBooks } from './redux/books/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+store.dispatch(fetchBooks());
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
